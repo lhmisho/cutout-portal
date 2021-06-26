d7821cbc34fd394fcec8953fd7f079323a5ff465
lhmisho <lhmisho@gmail.com>
2021-06-26 06:01:51 +0600

    Instructions model created

--- portal/models.py
+++ portal/models.py
@@ -31,3 +31,21 @@ class Addons(BaseModel):
 
     def __str__(self):
         return self.title
+
+
+class Instruction(BaseModel):
+    job_title = models.CharField(max_length=100)
+    requirements = models.ForeignKey(Requirement, on_delete=models.CASCADE)
+    addons = models.ForeignKey(Addons, on_delete=models.CASCADE)
+
+
+# class Order(BaseModel):
+#     image_path = models.CharField(max_length=200)
+#     image_quantity = models.IntegerField(null=True, blank=True, default=None)
+#     IMAGE_TYPE = [(1, 'Image'), (2, 'Portrait/Headshot/Model', (3, 'Others'))]
+#     image_type = models.IntegerField(choices=IMAGE_TYPE, default=1)
+#     save_metadata = models.BooleanField(default=False)
+#     instructions = models.ForeignKey(Instruction, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.id
